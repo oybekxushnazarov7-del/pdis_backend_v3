0345556b91c3df09b0dc7b07f31667f18819a34b
Oybek Xushnazarov <oybekxushnazarov7@gmail.com>
2026-04-18 15:40:25 +0500

    Real table creation logic added

--- app/main.py
+++ app/main.py
@@ -1,9 +1,30 @@
 from app.db import get_connection
-
 from fastapi import FastAPI
 from app.routes import users, expenses
 
 app = FastAPI()
+
+conn = get_connection()
+cursor = conn.cursor()
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT,
+        email TEXT UNIQUE
+    )
+""")
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS expenses (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        user_id INTEGER,
+        amount REAL,
+        category TEXT,
+        FOREIGN KEY (user_id) REFERENCES users(id)
+    )
+""")
+conn.commit()
+conn.close()
+# --------------------------------
 
 app.include_router(users.router)
 app.include_router(expenses.router)
