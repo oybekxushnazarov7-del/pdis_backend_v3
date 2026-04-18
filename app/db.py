f8674df4a69719b92730e1962bebbc66a8d0d0ba
Oybek Xushnazarov <oybekxushnazarov7@gmail.com>
2026-04-18 15:21:52 +0500

    Fix database path with os.path

--- app/db.py
+++ app/db.py
@@ -1,6 +1,11 @@
+import os
 import sqlite3
 
+# Loyihaning asosiy papkasini aniqlash
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+db_path = os.path.join(BASE_DIR, "database.db")
+
 def get_connection():
-    conn = sqlite3.connect("pdis.db")
+    conn = sqlite3.connect(db_path, check_same_thread=False)
     conn.row_factory = sqlite3.Row
     return conn
