f0f8b75c2033e02eb56a6e0af9ceb6e3b2e83f20
Oybek Xushnazarov <oybekxushnazarov7@gmail.com>
2026-04-20 06:25:56 +0500

    Use startup event for table creation

--- app/main.py
+++ app/main.py
@@ -1,31 +1,38 @@
-from app.db import get_connection
 from fastapi import FastAPI
+from app.db import get_connection
 from app.routes import users, expenses
 
 app = FastAPI()
 
-conn = get_connection()
-cursor = conn.cursor()
-cursor.execute("""
-    CREATE TABLE IF NOT EXISTS users (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        name TEXT,
-        email TEXT UNIQUE
-    )
-""")
-cursor.execute("""
-    CREATE TABLE IF NOT EXISTS expenses (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        user_id INTEGER,
-        amount REAL,
-        category TEXT,
-        FOREIGN KEY (user_id) REFERENCES users(id)
-    )
-""")
-conn.commit()
-conn.close()
-# --------------------------------
 
+def create_tables():
+    conn = get_connection()
+    cursor = conn.cursor()
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS users (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT,
+            email TEXT UNIQUE
+        )
+    """)
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS expenses (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            user_id INTEGER,
+            amount REAL,
+            category TEXT,
+            FOREIGN KEY (user_id) REFERENCES users(id)
+        )
+    """)
+    conn.commit()
+    conn.close()
+
+
+@app.on_event("startup")
+def on_startup():
+    create_tables()
+
+# Routerlarni ulash
 app.include_router(users.router)
 app.include_router(expenses.router)
 
